5016eee4ed48611b6e6fa412cca858464bfe5f3b
Rayen Bourguiba <58385581+RayenBourguiba@users.noreply.github.com>
2023-03-29 21:09:58 +0100

    static data manip

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,31 @@
+import memesData from "../memesData.js";
+import { useState } from "react";
+
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/9sw43.jpg",
+  });
+
+  const [allMemeImgs, setAllMemeImgs] = useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImgs.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+  }
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="formInput" placeholder="Top Text" />
         <input type="text" className="formInput" placeholder="Bottom Text" />
-        <button className="formButton">Generate A New Meme Image 👻</button>
-      </form>
+        <button className="formButton" onClick={getMemeImage}>
+          Generate A New Meme Image 👻
+        </button>
+      </div>
+      <img src={meme.randomImage} className="memeImage" />
     </main>
   );
 }
